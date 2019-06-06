55f2024fd56b1f7a78e9808e482b563cabeab454
rockstarcreativestudio <support@rockstarcreativestudio.com>
2019-06-06 10:46:58 -0400

    Add dep and update functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+const user = functions.config().sendgrid.user;
+const psword = functions.config().sendgrid.psword;
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: user,
+    pass: psword
+  }
+});
+
+exports.contact = functions.firestore
+  .document('contact-submissions/{contact}')
+  .onCreate((snap, context) => {
+    const newContact = snap.data();
+    const name = newContact.name;
+    const email = newContact.email;
+    const message = newContact.message;
+    const date = newContact.dateSubmitted;
+
+    try {
+      transporter.sendMail({
+        from: `"${name}" <${email}>`,
+        to: `"Megan Bailey" <megan@rockstarcreativestudio.com>`,
+        subject: 'New Contact Form Submission from Portfolio Website',
+        html: `New contact submission from the portfolio website.<br>\
+      Name: ${name}<br>\
+      Email: ${email}<br>\
+      Message: ${message}<br>\
+      Submitted on ${date}`
+      });
+    } catch (error) {
+      console.error('Something has went terribly wrong!', error);
+    }
+    return null;
+  });
